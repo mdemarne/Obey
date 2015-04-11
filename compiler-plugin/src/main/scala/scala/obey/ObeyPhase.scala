e46f864ce7e6b56b3fa78d13c4931723707fc8e9
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-04-11 15:21:13 +0200

    Removing var in ObeyPhase
    Using functional vals.

--- compiler-plugin/src/main/scala/scala/obey/ObeyPhase.scala
+++ compiler-plugin/src/main/scala/scala/obey/ObeyPhase.scala
@@ -22,6 +22,7 @@ trait ObeyPhase {
     val phaseName = "obey"
     override val runsAfter = List("typer")
     override val runsRightAfter = Some("convert")
+
     override def description = "apply obey rules"
 
 
@@ -31,37 +32,31 @@ trait ObeyPhase {
         val path = unit.source.path
         val originTree = unit.body.metadata("scalameta").asInstanceOf[scala.meta.Tree]
 
-        // Getting original tokens from source
+        /* Getting original tokens from source */
         val codec = scala.io.Codec(java.nio.charset.Charset.forName("UTF-8"))
         val content = scala.io.Source.fromFile(path)(codec).mkString
         val originTokens = content.tokens
 
-        val messageRules = UserOption.getReport
-        val formattingRules = UserOption.getFormat
-
-        var warnings: List[Message] = Nil // TODO: remove this var for a val
-
         /* Applying warnings */
-        if (!messageRules.isEmpty) {
-          warnings ++= messageRules.map(_.apply).reduce((r1, r2) => r1 +> r2)(originTree)
+        val simpleWarnings: List[Message] = UserOption.getReport match {
+          case lst if lst.isEmpty => Nil
+          case lst => lst.map(_.apply).reduce((r1, r2) => r1 +> r2)(originTree)
         }
 
-        var res: MatchResult[List[Message]] = null // TODO: remove this var for a val
-
-        /* Applying fixes */
-        if (!formattingRules.isEmpty) {
-          //reporter.info(NoPosition, "Fix Rules:\n"+formattingRules.mkString("\n"), true)
-          res = formattingRules.map(_.apply).reduce((r1, r2) => r1 + r2)(originTree)
-          if (res.tree.isDefined && !res.result.isEmpty) {
-            //Persistence.archive(path) // TODO: uncomment
-            val newTokens = formatter.Merge(originTokens, originTree, res.tree.get)
-            Persistence.persist(path + ".test", formatter.Print(newTokens)) // TODO: remove .test, here for testing
-            warnings ++= res.result.map(m => Message("[CORRECTED] " + m.message, m.tree))
-          } else {
-            warnings ++= res.result
-          }
+        /* Applying fixes, saving changes and returning warnings */
+        val fixWarnings: List[Message] = UserOption.getFormat match {
+          case lst if lst.isEmpty => Nil
+          case lst =>
+            val res = lst.map(_.apply).reduce((r1, r2) => r1 + r2)(originTree)
+            if (res.tree.isDefined && !res.result.isEmpty) {
+              //Persistence.archive(path) // TODO: uncomment
+              val newTokens = formatter.Merge(originTokens, originTree, res.tree.get)
+              Persistence.persist(path + ".test", formatter.Print(newTokens)) // TODO: remove .test, here for testing
+              res.result.map(m => Message("[CORRECTED] " + m.message, m.tree))
+            } else res.result
         }
-        warnings.foreach(m => reporter.warning(m.position, m.message))
+
+        (simpleWarnings ++ fixWarnings).foreach(m => reporter.warning(m.position, m.message))
       }
     }
   }
