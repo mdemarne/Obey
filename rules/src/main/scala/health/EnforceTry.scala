fd990ba4b7631e5a28ebe279f9e7a85b05064466
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-05-20 18:13:50 +0200

    BF: fixing EnforceTry rule

--- rules/src/main/scala/health/EnforceTry.scala
+++ rules/src/main/scala/health/EnforceTry.scala
@@ -8,9 +8,10 @@ import scala.obey.model._
 @Tag("Scala", "Style") object EnforceTry extends WarnRule {
 
   def description = "Enforce usage of Try object, preventing usage of conventional try/catch."
+  def message(t: Term) = Message(s"Instead of try/catch, use the Try object", t)
 
   def apply = collect {
-    case t: Term.TryWithCases =>
-      Message(s"Instead of try/catch, use the Try object", t)
+    case t: Term.TryWithCases => message(t)
+    case t: Term.TryWithTerm => message(t) 
   }.topDown
 }
