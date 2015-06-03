5b15f58613deeae03030d8c6dd33e31863a895f5
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-06-03 15:39:35 +0200

    Using quasiquote in migration rule

--- rules/src/main/scala/transformations/Play23GlobalControllerMigration.scala
+++ rules/src/main/scala/transformations/Play23GlobalControllerMigration.scala
@@ -1,8 +1,10 @@
-package statistics
+package transformations
 
+import scala.meta._
+import scala.meta.dialects.Scala211
 import scala.meta.tql._
 
-import scala.meta.internal.ast._
+import scala.meta.internal.{ ast => impl }
 import scala.obey.model._
 
 @Tag("Scala-play-migration23") object Play23GlobalControllerMigration extends StatRule {
@@ -11,23 +13,23 @@ import scala.obey.model._
 
   def apply = transformDefs andThen addImport
 
-  /* Transform all definitions to return a future */
+  // Force synthetic tokens for all tree nodes using TQL
   val transformDefs = (focus {
-    case Defn.Object(_, _, _, Template(_, parents, _, _)) =>
+    case impl.Defn.Object(_, _, _, impl.Template(_, parents, _, _)) =>
       parents.exists(p => p match {
-        case pp: Name => pp.value == "GlobalSettings"
+        case pp: impl.Name => pp.value == "GlobalSettings"
         case _ => true
       })
   } andThen (transform {
-    case origin @ Defn.Def(mods, _, _, _, tpe, body) if mods.exists(_.isInstanceOf[Mod.Override]) && body.isInstanceOf[Term.Block] =>
-      val newBody = Term.Apply(Term.Select(Term.Name("Future"), Term.Name("successful")), List(body))
-      origin.copy(body = newBody, decltpe = None) andCollect Message("Changing global controller return type for migration to Play 2.3", origin)
+    case x @ impl.Defn.Def(mods, _, _, _, tpe, body) if mods.exists(_.isInstanceOf[impl.Mod.Override]) && body.isInstanceOf[impl.Term.Block] =>
+      val newBody = q"Future.successful($body)".asInstanceOf[impl.Term]
+      x.copy(body = newBody, decltpe = None) andCollect Message("Changing global controller return type for migration to Play 2.3", x)
   }).topDown).topDown
 
   /* Add an import clause for concurrent.Future if required */
   val addImport = (transform {
-    case origin: Source if !origin.stats.exists(x => x.isInstanceOf[Import] && x.tokens.contains("Future") && x.tokens.contains("concurrent")) =>
-      val importFuture = Import(List(Import.Clause(Term.Select(Term.Name("scala"), Term.Name("concurrent")), List(Import.Selector.Name(Name.Indeterminate("Future"))))))
-      origin.copy(stats = importFuture +: origin.stats) andCollect Message("Adding an import of Future for global controller migration to Play 2.3", origin)
+    case s: impl.Source if !s.stats.exists(x => x.isInstanceOf[impl.Import] && x.tokens.contains("Future") && x.tokens.contains("concurrent")) =>
+      val importFuture = q"import scala.concurrent.Future".asInstanceOf[impl.Stat]
+      s.copy(stats = importFuture +: s.stats) andCollect Message("Adding an import of Future for global controller migration to Play 2.3", s)  
   }).topDown
 }
