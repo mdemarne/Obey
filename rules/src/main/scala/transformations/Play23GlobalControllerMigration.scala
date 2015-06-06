4c840fef7a56300cb7bb7242e7a6d804d14ec5ea
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-06-06 16:57:02 +0200

    Adding more tags in dummy migration rule for Play

--- rules/src/main/scala/transformations/Play23GlobalControllerMigration.scala
+++ rules/src/main/scala/transformations/Play23GlobalControllerMigration.scala
@@ -7,7 +7,7 @@ import scala.meta.tql._
 import scala.meta.internal.{ ast => impl }
 import scala.obey.model._
 
-@Tag("Scala-play-migration23") object Play23GlobalControllerMigration extends StatRule {
+@Tag("Scala-play-migration23", "Migration", "Demo") object Play23GlobalControllerMigration extends StatRule {
 
   def description = "Migrate the global controller of a play application to the newer version of Play (2.3)"
 
